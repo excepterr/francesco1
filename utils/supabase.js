580e0641cde1db7ca79599e494712e61b90a8575
excepterr <143307315+excepterr@users.noreply.github.com>
2026-09-24 15:04:37 +0900

    Delete utils directory
    Signed-off-by: excepterr <143307315+excepterr@users.noreply.github.com>

--- utils/supabase.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const config = require('../config.json');
-
-const supabase = createClient(config.supabaseUrl, config.supabaseKey);
-
-module.exports = supabase;
